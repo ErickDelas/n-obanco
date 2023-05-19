a76fe5a4e3540ad4512416cca9da6f7076c7e6d8
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-19 18:31:52 -0300

    Update NoBankTest.java

--- NoBankTest.java
+++ NoBankTest.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class NoBankTest {
 
     public static void main(String[] args) {
+        App app = new App();
         int opcao = -1;  
         Scanner leitor = new Scanner(System.in);
         while(opcao != 0){
-            App.exibeMenu();
+            App.menuInicial();
             opcao = leitor.nextInt();
             
             if(opcao == 0){
                 break;
-            }else{                
-                App.executaOpcao(opcao);
+            } else{
+                app.executaOpcao(opcao);
+            }
+            if(opcao == 2){
+                App.menuPrincipal();
+                opcao = leitor.nextInt();
+                app.executaOpcao(opcao);
             }
         }
-    }    
+    }
 }
