e313d42b9786f7091b809ae3f1ea19ea69caa374
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-19 18:31:11 -0300

    Update Credenciais.java

--- Credenciais.java
+++ Credenciais.java
@@ -3,11 +3,69 @@ package no.banktest;
 public class Credenciais {
     private String login;
     private String senha;
-    private 
+    private String nome;
+    private String cpf;
+    private String dtNascimento;
+    private String nCelular;
+    private String email;
 
+    public Credenciais(){
+        
+    }
+    
+    void apagarCredenciais(){
+        this.cpf = null;
+        this.dtNascimento = null;
+        this.email = null;
+        this.login = null;
+        this.nCelular = null;
+        this.nome = null;
+        this.senha = null;
+    }
+    
     public Credenciais (String login, String senha){
         this.login = login;
         this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDtNascimento() {
+        return dtNascimento;
+    }
+
+    public void setDtNascimento(String dtNascimento) {
+        this.dtNascimento = dtNascimento;
+    }
+
+    public String getnCelular() {
+        return nCelular;
+    }
+
+    public void setnCelular(String nCelular) {
+        this.nCelular = nCelular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getLogin() {
