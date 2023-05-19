d0b09056dcd8d780ec1abf83bd6605cac0e2b3ff
Matthewbene03 <134011399+Matthewbene03@users.noreply.github.com>
2023-05-19 18:30:33 -0300

    Update ContaBancaria.java

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -7,10 +7,15 @@ public class ContaBancaria {
     private String nConta;
     private LocalDate dataDeAbertura;
     private Credenciais credenciais; 
+    private App app;
     private double saldo;
     private int totContas = 0;
-    String dadosConta[][] = new String [100][2];
+    String dadosConta[][] = new String [100][7];
 
+    public ContaBancaria(){
+        this.credenciais = new Credenciais ();
+    }
+    
     public ContaBancaria(String nConta, LocalDate dataDeAbertura,
         Credenciais credenciais, double saldo){
         this.nConta = nConta;
@@ -24,51 +29,94 @@ public class ContaBancaria {
     }
     
     void cadastraConta(){
+        System.out.println("\n----- // ----- // ----- // ---- // ----- //");
+        
         System.out.println("Olá, vamos cadastrar uma conta nova para você.");
         
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe alguns dados para salvar no sistema: ");
-        System.out.println("CPF: ");
+        System.out.print("Nome: ");
+        String nome = sc.nextLine();
+        this.credenciais.setNome(nome);
+        
+        System.out.print("CPF: ");
         String cpf = sc.nextLine();
-        System.out.println("Data de nascimento: (dd/mm/aaaa/)");
-        int dia = sc.nextInt(); int mes = sc.nextInt(); int ano = sc.nextInt();
-        System.out.println("Você gostaria de receber informações sobre o nosso banco? [S/N]");
-        char resp = sc.next().charAt(0);
-        if(resp == 'S'){
-            System.out.println("Nos informe o seu email: ");
-            String email = sc.nextLine();
-            System.out.println("Agora, o seu numero de celular: ");
-            String nCelular = sc.nextLine();
-        }
+        this.credenciais.setCpf(cpf);
+        
+        System.out.print("Data de nascimento: (dd/mm/aaaa/)");
+        String dtNascimento = sc.nextLine();
+        this.credenciais.setDtNascimento(dtNascimento);
+        
+        System.out.println("Agora, iremos pedir alguns meios de contatos seu: ");
+        System.out.print("Nos informe o seu email: ");
+        String email = sc.nextLine();
+        this.credenciais.setEmail(email);
+        
+        System.out.print("Agora, o seu numero de celular: ");
+        String nCelular = sc.nextLine();
+        this.credenciais.setnCelular(nCelular);
+        
         System.out.println("Otimo, recebemos todos os seus dados. Agora, crie um usuario e senha.");
         System.out.print("Informe um usuario: ");
         String usuario = sc.nextLine();
         this.credenciais.setLogin(usuario);
+        
         System.out.print("Informe uma Senha: ");
         String senha = sc.nextLine();
         this.credenciais.setSenha(senha);
-        dadosConta[totContas][0] = this.credenciais.getLogin();
-        dadosConta[totContas][1] = this.credenciais.getSenha();
+        
+        dadosConta[totContas][0] = this.credenciais.getNome();
+        dadosConta[totContas][1] = this.credenciais.getCpf();
+        dadosConta[totContas][2] = this.credenciais.getDtNascimento();
+        dadosConta[totContas][3] = this.credenciais.getEmail();
+        dadosConta[totContas][4] = this.credenciais.getnCelular();
+        dadosConta[totContas][5] = this.credenciais.getLogin();
+        dadosConta[totContas][6] = this.credenciais.getSenha();
         
         totContas++;
-        System.out.print("-----Conta cadastrada com sucesso!-----");
+        System.out.print("-----Conta cadastrada com sucesso!-----\n");
     }
     
-    static void fazerLogin(){
+    void fazerLogin(){
         
-    }
-    
-    public void exibeConta(){
-        System.out.println("Conta Bancaria");
+        Scanner sc = new Scanner(System.in);
+        int i =0;
+        int j =0;
+        System.out.println("\n----- // ----- // ----- // ---- // ----- //");
         
-        for(int i = 0; i < totContas; i++){
-            System.out.printf("%d\tCPF: %s\tNome: %s\tN. SUS: %s"
-            + "\tDependente: %s\tCPF do dependente: %s\n", i, 
-            clientes[i][0], clientes[i][1], clientes[i][2], clientes[i][3], 
-            clientes[i][4]);
+        System.out.println("Olá, que bom ter você aqui novamente.");
+        while (i ==0){
+            System.out.print("Usuario: ");
+            String usuario = sc.next();
+            if(usuario.equals(this.credenciais.getLogin())){
+                System.out.print("Senha: ");
+                String senha = sc.next();
+                if(senha.equals(this.credenciais.getSenha())){
+                    System.out.println("Usuario e senha estão corretos!");
+//                    App.menuPrincipal();
+                    i++;
+                }
+            }
         }
     }
+    
+    void excluirConta(){
+        for(int i = 0; i < totContas; i++){
+            for(int j = 0; j<7; j++){
+                dadosConta[i][j] = null;
+            }
+        }
+        this.credenciais.apagarCredenciais();
     }
     
+    void exibeConta(){
+        System.out.println("----- Dados da sua Conta Bancaria -----");
+        
+        for(int i = 0; i < totContas; i++){
+            System.out.printf("%d - Nome: %s \tCPF: %s"
+                + "\tData de nascimento: %s \tEmail: %s \tNumero de celular: %s\t %s    %s\n"
+                , (i+1), dadosConta[i][0], dadosConta[i][1], dadosConta[i][2], 
+                dadosConta[i][3], dadosConta[i][4], dadosConta[i][5], dadosConta[i][6]);
+        }
+    }
 }
-
