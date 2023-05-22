c78f78bd75468d5c99712329d06d445a6fd3beb2
after001 <132841119+after001@users.noreply.github.com>
2023-05-21 22:44:25 -0300

    Update BancoDeDados.java

--- BancoDeDados.java
+++ BancoDeDados.java
@@ -3,9 +3,30 @@ package no.banktest;
 import java.util.ArrayList;
 
 public class BancoDeDados {
-    private ArrayList <ContaBancaria> conta;
-    
-    public BancoDeDados (){
-        this.conta = new ArrayList();
+    private ArrayList<ContaBancaria> contas;
+
+    public BancoDeDados() {
+        this.contas = new ArrayList<>();
+    }
+
+    public ArrayList<ContaBancaria> getContas() {
+        return contas;
+    }
+
+    public void adicionarConta(ContaBancaria conta) {
+        contas.add(conta);
+    }
+
+    public void removerConta(ContaBancaria conta) {
+        contas.remove(conta);
+    }
+
+    public void exibirContas() {
+        System.out.println("----- Lista de Contas Bancárias -----");
+        for (int i = 0; i < contas.size(); i++) {
+            System.out.println("Conta " + (i + 1));
+            contas.get(i).exibeConta();
+            System.out.println("-------------------------");
+        }
     }
 }
